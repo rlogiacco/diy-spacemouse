3ee636b3c0c79144014640d5d86cb56221a58a43
Roberto Lo Giacco <rlogiacco@gmail.com>
2024-06-13 01:10:34 +0200

    various improvements
    * used MicroDebug for data logging
    * switched to RGB LEDs WS2812
    * refactored to be more readable
    * added ability to reprogram without opening

--- Code/diy-spacemouse.ino
+++ Code/diy-spacemouse.ino
@@ -1,17 +1,34 @@
+
 #include <TinyUSB_Mouse_and_Keyboard.h>
 #include <OneButton.h>
 #include <Tlv493d.h>
 #include <SimpleKalmanFilter.h>
+#include <SerialDebug.h>
+#include <Adafruit_NeoPixel.h>
+
+//#define SERIAL_DEBUG false
+
+//Adafruit_NeoPixel onboard(1, 16, NEO_GBRW + NEO_KHZ800);
+Adafruit_NeoPixel pixels(3, 3, NEO_GRB + NEO_KHZ800);
 
 Tlv493d mag = Tlv493d();
-SimpleKalmanFilter xFilter(1, 1, 0.2), yFilter(1, 1, 0.2), zFilter(1, 1, 0.2);
+
+struct { SimpleKalmanFilter x,y,z;} filter = {
+      SimpleKalmanFilter(1, 1, 0.2),
+      SimpleKalmanFilter(1, 1, 0.2),
+      SimpleKalmanFilter(1, 1, 0.2)
+};
 
 // Setup buttons
-OneButton button1(27, true);
-OneButton button2(24, true);
+OneButton left(0, true);
+OneButton right(1, true);
+#define INTERVAL 10000
+unsigned long boot;
 
-float xOffset = 0, yOffset = 0, zOffset = 0;
-float xCurrent = 0, yCurrent = 0, zCurrent = 0;
+struct coords { float x,y,z; };
+
+coords offset = {0,0,0};
+coords current = {0,0,0};
 
 int calSamples = 300;
 int sensivity = 8;
@@ -24,125 +41,123 @@ float zThreshold = xyThreshold * 1.5;
 
 bool isOrbit = false;
 
-void setup()
-{
+void setup() {
+  SERIAL_DEBUG_SETUP(9600);
+  pixels.begin();
+  pixels.fill(pixels.Color(255,0,0));
+  pixels.show();
 
-  button1.attachClick(goHome);
-  button1.attachLongPressStop(goHome);
+  left.attachClick(goHome);
+  left.attachLongPressStart(bootsel);
+  left.attachLongPressStop(goHome);
 
-  button2.attachClick(fitToScreen);
-  button2.attachLongPressStop(fitToScreen);
+  right.attachClick(fitToScreen);
+  right.attachLongPressStart(bootsel);
+  right.attachLongPressStop(fitToScreen);
 
   // mouse and keyboard init
   Mouse.begin();
   Keyboard.begin();
 
-  Serial.begin(9600);
+  Wire1.setSDA(10);
+  Wire1.setSCL(11);
   Wire1.begin();
 
   // mag sensor init
   mag.begin(Wire1);
   mag.setAccessMode(mag.MASTERCONTROLLEDMODE);
   mag.disableTemp();
-
+  
   // crude offset calibration on first boot
-  for (int i = 1; i <= calSamples; i++)
-  {
-
+  pixels.fill(pixels.Color(0,0,255));
+  pixels.show();
+  DEBUG("Auto calibration in progress...");
+  for (int i = 1; i <= calSamples; i++) {
     delay(mag.getMeasurementDelay());
     mag.updateData();
-
-    xOffset += mag.getX();
-    yOffset += mag.getY();
-    zOffset += mag.getZ();
-
-    Serial.print(".");
+    offset = {offset.x + mag.getX(), offset.y + mag.getY(), offset.z + mag.getZ()};
   }
 
-  xOffset = xOffset / calSamples;
-  yOffset = yOffset / calSamples;
-  zOffset = zOffset / calSamples;
+  offset = { offset.x / calSamples, offset.y / calSamples, offset.z / calSamples };
 
-  Serial.println();
-  Serial.println(xOffset);
-  Serial.println(yOffset);
-  Serial.println(zOffset);
+  pixels.fill(pixels.Color(255,0,255));
+  pixels.show();
+  DEBUG("Calibration completed!");
+  DEBUG("X", offset.x, "Y", offset.y, "Z", offset.z);
+  boot = millis();
 }
 
-void loop()
-{
-
+void loop() {
   // keep watching the push buttons
-  button1.tick();
-  button2.tick();
+  left.tick();
+  right.tick();
+
+  if (millis() - boot > INTERVAL) {
+    left.attachLongPressStart(NULL);
+    right.attachLongPressStart(NULL);
+    pixels.fill(pixels.Color(0,255,0));
+    pixels.show();
+  }
 
   // get the mag data
   delay(mag.getMeasurementDelay());
   mag.updateData();
 
   // update the filters
-  xCurrent = xFilter.updateEstimate(mag.getX() - xOffset);
-  yCurrent = yFilter.updateEstimate(mag.getY() - yOffset);
-  zCurrent = zFilter.updateEstimate(mag.getZ() - zOffset);
+  current = { 
+      filter.x.updateEstimate(mag.getX() - offset.x), 
+      filter.y.updateEstimate(mag.getY() - offset.y),
+      filter.z.updateEstimate(mag.getZ() - offset.z)
+    };
 
   // check the center threshold
-  if (abs(xCurrent) > xyThreshold || abs(yCurrent) > xyThreshold)
-  {
+  if (abs(current.x) > xyThreshold || abs(current.y) > xyThreshold) {
+    
+    // map the magnetometer xy to the allowed 127 range in HID reports
+    int xMove = map(current.x, -inRange, inRange, -outRange, outRange);
+    int yMove = map(current.y, -inRange, inRange, -outRange, outRange);
 
-    int xMove = 0;
-    int yMove = 0;
-
-    // map the magnetometer xy to the allowed 127 range in HID repports
-    xMove = map(xCurrent, -inRange, inRange, -outRange, outRange);
-    yMove = map(yCurrent, -inRange, inRange, -outRange, outRange);
-
-    // press shift to orbit in Fusion 360 if the pan threshold is not corssed (zAxis)
-    if (abs(zCurrent) < zThreshold && !isOrbit)
-    {
+    // press shift to orbit in Fusion 360 if the pan threshold is not crossed (zAxis)
+    if (abs(current.z) < zThreshold && !isOrbit) {
       Keyboard.press(KEY_LEFT_SHIFT);
       isOrbit = true;
     }
 
-    // pan or orbit by holding the middle mouse button and moving propotionaly to the xy axis
+    // pan or orbit by holding the middle mouse button and moving proportionally to the xy axis
     Mouse.press(MOUSE_MIDDLE);
     Mouse.move(yMove, xMove, 0);
-  }
-  else
-  {
+  } else {
 
     // release the mouse and keyboard if within the center threshold
     Mouse.release(MOUSE_MIDDLE);
     Keyboard.releaseAll();
     isOrbit = false;
   }
-
-  Serial.print(xCurrent);
-  Serial.print(",");
-  Serial.print(yCurrent);
-  Serial.print(",");
-  Serial.print(zCurrent);
-  Serial.println();
+//  DEBUG("X", current.x, "Y", current.y, "Z", current.z);
 }
 
 // go to home view in Fusion 360 by pressing  (CMD + SHIFT + H) shortcut assigned to the custom Add-in command
-void goHome()
-{
+void goHome() {
   Keyboard.press(KEY_LEFT_GUI);
   Keyboard.press(KEY_LEFT_SHIFT);
   Keyboard.write('h');
-
   delay(10);
   Keyboard.releaseAll();
-  Serial.println("pressed home");
+
+  DEBUG("Pressed HOME");
 }
 
 // fit to view by pressing the middle mouse button twice
-void fitToScreen()
-{
+void fitToScreen() {
   Mouse.press(MOUSE_MIDDLE);
   Mouse.release(MOUSE_MIDDLE);
   Mouse.press(MOUSE_MIDDLE);
   Mouse.release(MOUSE_MIDDLE);
 
-  Serial.println("pressed fit");
+  DEBUG("Pressed FIT");
+}
+
+// reboot into programming mode
+void bootsel() {
+  reset_usb_boot(0, 0);
 }
